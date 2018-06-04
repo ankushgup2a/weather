1a2e0967b40c55166eb162a3cf3286af0c377e03
ankushgup2a <ankush@91mobiles.com>
2018-06-04 23:11:43 +0530

    Update webpack.config.js
    Add port 80

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ module.exports = {
     historyApiFallback: true,
     contentBase: './',
     host: "0.0.0.0",
+    port: 80,
     disableHostCheck: true,
     watchOptions: {
       aggregateTimeout: 300,
