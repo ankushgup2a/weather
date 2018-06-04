d4341034a3c90e3b8c5be2b3de4487bbb7d4c5c6
ankushgup2a <ankush@91mobiles.com>
2018-06-04 23:18:29 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,6 @@ module.exports = {
     historyApiFallback: true,
     contentBase: './',
     host: "0.0.0.0",
-    port: 80,
     disableHostCheck: true,
     watchOptions: {
       aggregateTimeout: 300,
